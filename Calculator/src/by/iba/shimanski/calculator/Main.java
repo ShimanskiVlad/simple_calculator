17287ea73e5a2f5dc4c13d919d80df04cff61f9b
Vlad Shimanski <vladshimanski@gmail.com>
2016-02-15 13:25:59 +0300

    add multiplc, div, menu

--- Calculator/src/by/iba/shimanski/calculator/Main.java
+++ Calculator/src/by/iba/shimanski/calculator/Main.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
 public class Main {	
 	
 	public static void main(String[] args) {
-		double d1 = MyInput.inputDouble();
-		double d2 = MyInput.inputDouble();
-		Operation operation = MyInput.inputOperation();
-		System.out.println(d1 + " " + operation + " " + d2 + " = " + Calculator.calculate(d1, d2, operation));
+		CalculatorView calculatorView = new CalculatorView();
+		calculatorView.go();
 	}
 
 }
